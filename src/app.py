86e48ff5994feb137d42f105ce51cb11527557c8
javeriahanif <hanifj@vcu.edu>
2023-12-10 07:18:00 -0500

    added create, read, update for playlist

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, text
@@ -209,11 +210,97 @@ def get_albums():
 
 @app.route('/playlists', methods=['GET'])
 def get_playlists():
-    query = text("SELECT ID, name, description, TIME_FORMAT(duration, '%H:%i') AS duration, user_ID FROM playlist;")
-    playlist_list = testExecution(db.session.execute(query))
-    if(playlist_list):
-        return jsonify(playlists=playlist_list)
-    return jsonify(message='No playlists have been added.'), 404
+    # get parameters 
+    id_regex = request.args.get('id')
+    name_regex = request.args.get('name')
+    description_regex = request.args.get('description')
+    duration_regex = request.args.get('duration')
+    #user_regex = request.args.get('user')
+    user_id_regex = request.args.get('user_id')
 
+    # build the query 
+    query = text("SELECT playlist.ID, playlist.name, playlist.description, TIME_FORMAT(playlist.duration, '%H:%i') AS duration, playlist.user_ID, user.user_name AS user_name FROM playlist JOIN user ON playlist.user_ID = user.ID WHERE"
+                "(playlist.ID REGEXP :id_regex OR :id_regex IS NULL) AND"
+                "(playlist.name REGEXP :name_regex OR :name_regex IS NULL) AND"
+                "(playlist.description REGEXP :description_regex OR :description_regex IS NULL) AND"
+                "(playlist.duration REGEXP :duration_regex OR :duration_regex IS NULL) AND"
+                #"(user.user_name REGEXP :user_regex OR user_regex IS NULL) AND" 
+                "(user.ID REGEXP :user_id_regex OR :user_id_regex IS NULL);")
+    # execute the query 
+    result =  testExecution(db.session.execute(query, {'id_regex': id_regex, 'name_regex': name_regex, 'description_regex': description_regex, 'duration_regex': duration_regex, 'user_id_regex': user_id_regex}))
+
+
+    # get result
+    #playlists_list = [{'ID': playlist.ID, 'name': playlist.name, 'description': playlist.description, 'duration': playlist.duration, 'user_ID': playlist.user_ID, 'Username': playlist.user_name} for playlist in result]
+    playlists_list = [{'ID': playlist['ID'], 'name': playlist['name'], 'description': playlist['description'], 'duration': playlist['duration'], 'user_ID': playlist['user_ID'], 'Username': playlist['user_name']} for playlist in result]
+
+    if playlists_list:
+        return jsonify(playlists_list)
+    return jsonify(message='No playlists match the criteria.'), 404
+
+@app.route('/playlists/<int:playlist_id>', methods=['GET'])
+def get_playlist_by_id(playlist_id):
+    query = text("SELECT ID, name, description, TIME_FORMAT(duration, '%H:%i') AS duration, user_ID FROM playlist WHERE id = :playlist_id;")
+    playlist_list = testExecution(db.session.execute(query, {'playlist_id': playlist_id}))
+    if playlist_list:
+        return jsonify(playlists=playlist_list)
+    return jsonify(message='Playlist not found.'), 404
+
+@app.route('/playlists/user/<int:user_id>', methods=['GET'])
+def get_playlists_by_user_id(user_id):
+    query = text("SELECT ID, name, description, TIME_FORMAT(duration, '%H:%i') AS duration, user_ID FROM playlist WHERE user_ID = :user_id;")
+    playlist_list = testExecution(db.session.execute(query, {'user_id': user_id}))
+    if playlist_list:
+        return jsonify(playlists=playlist_list)
+    return jsonify(message='No playlists found for the user ID.'), 404
+
+@app.route('/add_playlist', methods=['POST'])
+def add_playlist():
+    # request JSON
+    data = request.json
+    name = data.get('name')
+    description = data.get('description', None)
+    duration = data.get('duration')
+    user_ID = data.get('user_ID')
+
+    if not name or not duration or not user_ID:
+        return jsonify({'error': 'Missing required fields for adding a playlist (name, duration, user_ID)'}), 400
+
+    # build     query
+    query = text("INSERT INTO playlist (name, description, duration, user_ID) VALUES (:name, :description, :duration, :user_ID);")
+    # execute the query to insert a new playlist
+    db.session.execute(query, {'name': name, 'description': description, 'duration': duration, 'user_ID': user_ID})
+    db.session.commit()
+
+    # get new playlist 
+    query_get = text("SELECT ID, name, description, TIME_FORMAT(duration, '%H:%i') AS duration, user_ID FROM playlist WHERE name = :name;")
+    playlist_list = testExecution(db.session.execute(query_get, {'name': name}))
+
+    return jsonify({'message': 'Playlist added successfully', 'playlist': playlist_list[0]}), 201
+
+@app.route('/update_playlist/<int:playlist_id>', methods=['PUT'])
+def update_playlist(playlist_id):
+    # request JSON
+    data = request.json
+    name = data.get('name')
+    description = data.get('description', None)
+    duration = data.get('duration')
+    user_ID = data.get('user_ID')
+
+    if not name or not duration or not user_ID:
+        return jsonify({'error': 'Missing required fields for updating a playlist (name, duration, user_ID)'}), 400
+
+    query = text("UPDATE playlist SET name = :name, description = :description, duration = :duration, user_ID = :user_ID WHERE ID = :playlist_id;")
+    db.session.execute(query, {'name': name, 'description': description, 'duration': duration, 'user_ID': user_ID, 'playlist_id': playlist_id})
+    db.session.commit()
+
+    logging.info('Executing update_playlist method...')
+    logging.debug(f'Input data: {data}')
+
+    query_get = text("SELECT ID, name, description, TIME_FORMAT(duration, '%H:%i') AS duration, user_ID FROM playlist WHERE ID = :playlist_id;")
+    playlist_list = testExecution(db.session.execute(query_get, {'playlist_id': playlist_id}))
+
+    return jsonify({'message': 'Playlist updated successfully', 'playlist': playlist_list[0]}), 200
+    
 if __name__ == '__main__':
     app.run(debug=True)
